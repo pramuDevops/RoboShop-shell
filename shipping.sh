84e92922179d58134a3c86e8f080b3677434a495
pramodini <pramodinithurpu95@gmail.com>
2025-07-14 12:28:20 -0500

    roboshop code

--- shipping.sh
+++ shipping.sh
@@ -5,7 +5,6 @@ mkdir /app
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
 cd /app
 unzip /tmp/shipping.zip
-cd /app
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 dnf install mysql -y
