aa6f89cea0c812629634620b92c59d15b71f6279
pramodini <pramodinithurpu95@gmail.com>
2025-07-22 17:25:25 -0500

    roboshop code

--- cart.sh
+++ cart.sh
@@ -1,13 +1,3 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
-cp cart.service /etc/systemd/system/cart.service
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
-cd /app
-unzip /tmp/cart.zip
-cd /app
-npm install
-systemctl enable cart
-systemctl start cart
+ source common.sh
+ component=cart
+ NODEJS
